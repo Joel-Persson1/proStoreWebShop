a203f72323a258b12428229a3f1f91371ba8521f
Joel-Persson1 <jolle.ov@gmail.com>
2025-08-12 09:22:00 +0200

    Built up the foundation of the application using layouts, pages and components. I have started to develop the header and footer for all screen devices and moving forward to the main content of the homepage.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import sampleData from "@/db/sample-data";
+
+import ProductList from "@/components/shared/product/product-list";
+
+const Homepage = () => {
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      ></ProductList>
+    </>
+  );
+};
+
+export default Homepage;
