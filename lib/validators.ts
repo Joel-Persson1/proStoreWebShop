79a66531f0caf22017e7039fa2d8f7f57a20fca5
Joel-Persson1 <jolle.ov@gmail.com>
2025-08-13 12:58:15 +0200

    Made zod SChema for signing up, Made server actions for signing up, You can now sign up on the server side

--- lib/validators.ts
+++ lib/validators.ts
@@ -29,3 +29,19 @@ export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(6, "Password must be atleast 6 characters"),
 });
+
+// Schema for signing up a user
+
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be atleast 3 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be atleast 6 characters"),
+    confirmPassword: z
+      .string()
+      .min(6, "Confirm Password must be atleast 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+  });
