96611375e95a110717ccd927def89c20798894d9
Joel-Persson1 <jolle.ov@gmail.com>
2025-08-14 15:37:22 +0200

    Added shipping address form. When you have all the items in the cart you can click continue and get sent to the shipping address form

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -74,3 +74,12 @@ export async function SignUpUser(prevState: unknown, formData: FormData) {
     return { success: false, message: formatError(error) };
   }
 }
+
+// Get user by ID
+export async function getUserById(userId: string) {
+  const user = await prisma.user.findFirst({
+    where: { id: userId },
+  });
+  if (!user) throw new Error("User not found");
+  return user;
+}
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -18,3 +18,11 @@ export const signUpDefaultValues = {
   password: "",
   confirmPassword: "",
 };
+
+export const shippingAddressDefaultValues = {
+  fullName: "John Dow",
+  streetAddress: "123 Main Street",
+  city: "Anytown",
+  postalCode: "12345",
+  country: "USA",
+};
