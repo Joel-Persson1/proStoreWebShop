e73e153af2110f96cce7bdc506ac67bff5ff8fff
Joel-Persson1 <jolle.ov@gmail.com>
2025-08-14 11:03:22 +0200

    Added UI functionality to the add to cart button. You can now add and remove from the cart. If the cart has the item then you can change the quantity by clicking the plus or minus buttons on the product page

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,15 +1,16 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { Plus, Minus } from "lucide-react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
-import { CartItem } from "@/types";
+import { Cart, CartItem } from "@/types";
 import { toast } from "sonner";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
   const router = useRouter();
 
+  // Handles the add to cart event
   const handleAddToCart = async () => {
     const res = await addItemToCart(item);
 
@@ -26,7 +27,32 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
+  // Handles the remove from cart event
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    if (res.success) {
+      toast(res.message);
+    } else {
+      toast.error(res.message);
+    }
+  };
+
+  // Check if item is in cart
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="h-4 w-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
       <Plus /> Add To Cart
     </Button>
